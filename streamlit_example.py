8ab177c33ff1b98ed2cddc0d011a787dd417db20
Demehin Ibukun <ibk2k7@gmail.com>
2020-05-18 20:05:27 +0100

    streamlit first commit example

---
+++ streamlit_example.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("This is a simple example to deploy on heroku")
+    st.subheader("Streamlit - Heroku example")
+
+if __name__ =='__main__':
+    main()
